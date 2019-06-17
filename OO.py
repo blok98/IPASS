80fedf3842c1c7b1b047951352e6353a6fe8306f
tom_s <t.blokland.1998@gmail.com>
2019-06-17 16:30:09 +0200

    OO

--- OO.py
+++ OO.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
+import unidecode
 from time import sleep
+
 
 os.chdir('C:\\Users\\tom_s\\Desktop')  # Set wd
 df1 = pd.read_csv("All_data.csv", index_col=False, encoding="unicode_escape", sep=";")
@@ -9,16 +11,18 @@ match_data = df1  # Save a copy
 fifa_data = df2
 
 
+
+
 class Match:
-  home_Team = 'None'
-  out_Team = 'None'
-  won = -1 # 1 = home team, 0 = out team
+  home_team = 'None'
+  away_team = 'None'
+  won = -1 # 1 = home team, 0 = out team, -1 = draw
   date = ""
   league = ""
 
-  def __init__(self, home_team,out_team,won,date):
+  def __init__(self, home_team,away_team,won,date):
       self.home_team = home_team
-      self.out_team = out_team
+      self.away_team = away_team
       self.won = won
       self.date = date
       self.league = league
@@ -60,18 +64,32 @@ class Player:
 
 player_coll=[]
 team_coll=[]
+match_coll=[]
 
 for i in match_data["Nr"]:
-    league=match_data["Div"][i]
-    date=match_data["Date"][i]
     HomeTeam=match_data["HomeTeam_decoded"][i]
     AwayTeam=match_data["AwayTeam_decoded"][i]
-    won=match_data["FTHG"][i]>match_data["FTAG"][i]
+    won=int(match_data["FTHG"][i]>match_data["FTAG"][i])
+    if match_data["FTHG"][i]==match_data["FTAG"][i]:
+        won=-1
+    date=match_data["Date"][i]
+    league = match_data["Div"][i]
+    match_list=[HomeTeam,AwayTeam,won,date,league]
     if HomeTeam not in team_coll:
         team_coll.append(HomeTeam)
+    if AwayTeam not in team_coll:
+        team_coll.append(AwayTeam)
+    if match_list not in match_coll:
+        match_coll.append(match_list)
 
+#for each teamname in list team_coll, replace that element with object Team(teamname)
 for i in range(len(team_coll)):
     team_coll[i]=Team(team_coll[i])
+
+#for each attribute in each element of match_list, replace element with object Match(att[0],att[1],att[2]...)
+for i in range(len(match_coll)):
+    att=match_coll[i]
+    match_coll[i]=Match(att[0],att[1],att[2],att[3])
 
 for i in fifa_data["Nr"]:
     name=fifa_data["Name"][i]
@@ -86,22 +104,32 @@ for i in fifa_data["Nr"]:
             team.add_player(player)
             team_coll[i]=team
 
-
-index=0
-for i in team_coll:
-    print(i)
-    for j in i.get_players():
-        print("  ",j.__dict__)
-    index+=1
-    if index==10:
-        break
-print(str(index)+" resultaten gevonden")
+for i in match_coll:
+    if i.home_team=="Manchester City" and i.away_team=="Manchester United":
+        print(i.__dict__)
 
 
+# for i in team_coll:
+#     if i.name=="Real Madrid":
+#         print(i.__dict__)
+#         for j in i.get_players():
+#             print(j)
 
 
 
 
+
+
+## print all clubs with belonging players
+# index=0
+# for i in team_coll:
+#     print(i)
+#     for j in i.get_players():
+#         print("  ",j.__dict__)
+#     index+=1
+# print(str(index)+" resultaten gevonden")
+
+# # simple test
 # p1=Player("jan","barcelona",29,94)
 # p2=Player("han","city",32,91)
 # p3=Player("hendrik","barcelona",22,88)
